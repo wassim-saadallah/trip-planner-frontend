b7965960f0bdb06f54b87298a5f4e0ac51e9c844
wassim-saadallah <wassim.saadalla@gmail.com>
2019-07-17 17:09:58 +0100

    bootstrap template

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$("#menu-toggle").click(function (e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
